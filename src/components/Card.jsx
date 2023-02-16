0488e2295f1aa84c9198f10f112bd0556539f04d
Bagher hosseini <bagherhosseini03@gmail.com>
2023-02-16 23:55:38 +0100

    lightning

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,11 +8,14 @@ export default function Card(){
     const [city, setCity] = useState();
     const [Latitude, setLatitude] = useState(0);
     const [longitude, setLongitude] = useState(0);
+    const [clear, setClear] = useState('none');
+    const [cloudy, setCloudy] = useState('none');
+    const [raining, setRaining] = useState('none');
+    const [snowing, setSnowing] = useState('none');
 
 
     useEffect(() => {
         const url = `https://geocoding-api.open-meteo.com/v1/search?name=stockholm`;
-
         async function fetchDataCity() {
             const response = await fetch(url);
             const data = await response.json();
@@ -20,27 +23,27 @@ export default function Card(){
             setLatitude(data.results[0].latitude);
             setLongitude(data.results[0].longitude);
         }
-
         fetchDataCity();
     }, []);
 
-    let cityLatitude = 0;
-    let citylongitude = 0;
-    if(city){
-        cityLatitude = Latitude;
-        citylongitude = longitude;
+    
+    useEffect(() => {
+        let cityLatitude = 0;
+        let citylongitude = 0;
+        if(city){
+            cityLatitude = Latitude;
+            citylongitude = longitude;
 
-        //check MET Norway API
-        //check weather code in the end of this https://open-meteo.com/en/docs page
-        const url = `https://api.open-meteo.com/v1/gfs?latitude=${cityLatitude}&longitude=${citylongitude}&hourly=temperature_2m`
-        
-        fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            setWeather(data);
-        });
-        
-    }
+            //check MET Norway API
+            //check weather code in the end of this https://open-meteo.com/en/docs page
+            const url = `https://api.open-meteo.com/v1/forecast?latitude=${cityLatitude}&longitude=${citylongitude}&hourly=temperature_2m,weathercode`;
+            fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setWeather(data);
+            });
+        }
+    }, [Latitude, longitude, city]);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -55,6 +58,49 @@ export default function Card(){
 
     if(weather.hourly){
         temperature = weather.hourly.temperature_2m[currentHour];
+        let weatherCode = weather.hourly.weathercode[currentHour];
+
+        switch (true) {
+            case (weatherCode === 0):
+                console.log('Clear sky')
+                break;
+
+            case (weatherCode >= 1 && weatherCode <= 3):
+                // 1, 2, 3	Mainly clear, partly cloudy, and overcast
+                console.log('Cloudy')
+                break;
+
+            case (weatherCode >= 45 && weatherCode <= 48):
+                console.log('Fog')
+                break;
+
+            case (weatherCode >= 51 && weatherCode <= 57):
+                console.log('Drizzle')
+                break;
+
+            case (weatherCode >= 61 && weatherCode <= 67):
+                console.log('Raining')
+                break;
+
+            case (weatherCode >= 80 && weatherCode <= 82):
+                console.log('Raining')
+                break;
+
+            case (weatherCode >= 71 && weatherCode <= 77):
+                console.log('Snowing')
+                break;
+
+            case (weatherCode >= 85 && weatherCode <= 86):
+                console.log('Snowing')
+                break;
+            
+            case (weatherCode >= 95 && weatherCode <= 99):
+                console.log('Thunderstorm')
+                break;
+
+            default:  
+                break;
+        }
     }
 
     return(
@@ -74,8 +120,26 @@ export default function Card(){
                         </div>
 
                         <div className='weatherAnimation'>
+                            
+                            <div className='suny' style={{ display: clear }}>
+                                <div className="forecast">
+                                    <div className="forecast__sunny"></div>
+                                </div>
+                            </div>
 
-                            <div className='rain'>
+                            <div className='cloudy' style={{ display: cloudy }}>
+                                <div className="forecast">
+                                    <div className="forecast__cloudy">
+                                        <div className="forecast__cloudy__sun"></div>
+                                        <div className="forecast__cloudy__cloud forecast__cloudy__cloud--small">
+                                        </div>
+                                        <div className="forecast__cloudy__cloud forecast__cloudy__cloud--normal">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='rain' style={{ display: raining }}>
                                 <div className="forecast">
                                     <div className="forecast__rainy">
                                         <div className="forecast__rainy__rain forecast__rainy__rain--one">
@@ -98,7 +162,7 @@ export default function Card(){
                                 </div>
                             </div>
 
-                            <div className='snow'>
+                            <div className='snow' style={{ display: snowing }}>
                                 <div className="forecast">
                                     <div className="forecast__snow">
                                         <div className="forecast__snow__snow forecast__snow__snow--first"><i className="fa-regular fa-snowflake"></i></div>
@@ -113,23 +177,16 @@ export default function Card(){
                                 </div>
                             </div>
 
-                            <div className='suny'>
+                            <div className='storm' style={{ display: 'block' }}>
                                 <div className="forecast">
-                                    <div className="forecast__sunny"></div>
-                                </div>
-                            </div>
-
-                            <div className='cloudy'>
-                                <div className="forecast">
+                                    <div className='bowie'></div>
                                     <div className="forecast__cloudy">
-                                        <div className="forecast__cloudy__sun"></div>
-                                        <div className="forecast__cloudy__cloud forecast__cloudy__cloud--small">
-                                        </div>
                                         <div className="forecast__cloudy__cloud forecast__cloudy__cloud--normal">
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
                     </div>
 
