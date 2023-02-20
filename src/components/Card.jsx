6439e2cd2189b50467328db986a4a291b5da9e1a
Bagher hosseini <bagherhosseini03@gmail.com>
2023-02-20 19:16:12 +0100

    fix switch

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,42 +1,52 @@
 import '../styles/style.scss';
 import React, { useState, useEffect } from "react";
-import CoordinateInput from 'react-coordinate-input';
+import Weatherdisplay from './Futureweather.jsx';
+import Weathertoday from './Weathertoday.jsx';
 
 export default function Card(){
     const [weather, setWeather] = useState({});
     const [currentHour, setCurrentHour] = useState(0);
-    const [city, setCity] = useState();
+
+    const [citydata, setCitydata] = useState();
     const [Latitude, setLatitude] = useState(0);
     const [longitude, setLongitude] = useState(0);
-    const [clear, setClear] = useState('none');
-    const [cloudy, setCloudy] = useState('none');
-    const [raining, setRaining] = useState('none');
-    const [snowing, setSnowing] = useState('none');
+    const [city, setCity] = useState();
+    const [cityName, setCityName] = useState("");
 
 
-    useEffect(() => {
-        const url = `https://geocoding-api.open-meteo.com/v1/search?name=stockholm`;
-        async function fetchDataCity() {
-            const response = await fetch(url);
-            const data = await response.json();
-            setCity(data);
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setCityName(city)
+
+        try {
+          const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
+          const data = await response.json();
+    
+          if (data) {
+            setCitydata(data);
             setLatitude(data.results[0].latitude);
             setLongitude(data.results[0].longitude);
+          } else {
+            setCitydata();
+          }
+        } catch (error) {
+          console.error(error);
         }
-        fetchDataCity();
-    }, []);
+    };
+
+    function handleChange(event) {
+        setCity(event.target.value);
+    }
 
     
     useEffect(() => {
         let cityLatitude = 0;
         let citylongitude = 0;
-        if(city){
+        if(citydata){
             cityLatitude = Latitude;
             citylongitude = longitude;
-
-            //check MET Norway API
-            //check weather code in the end of this https://open-meteo.com/en/docs page
-            const url = `https://api.open-meteo.com/v1/forecast?latitude=${cityLatitude}&longitude=${citylongitude}&hourly=temperature_2m,weathercode`;
+            //https://api.open-meteo.com/v1/dwd-icon?latitude=${cityLatitude}&longitude=${citylongitude}&hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min&windspeed_unit=ms&timezone=Europe%2FBerlin
+            const url = `https://api.open-meteo.com/v1/dwd-icon?latitude=${cityLatitude}&longitude=${citylongitude}&hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min&windspeed_unit=ms&timezone=Europe%2FBerlin`;
             fetch(url)
             .then((res) => res.json())
             .then((data) => {
@@ -55,143 +65,57 @@ export default function Card(){
     }, []);
 
     let temperature = 0;
+    let humidity = 0;
+    let wind = 0;
+
+    let Fweather =  Weatherdisplay(weather.daily);
+
+    let Tweather = null;
 
     if(weather.hourly){
         temperature = weather.hourly.temperature_2m[currentHour];
+        humidity = weather.hourly.relativehumidity_2m[currentHour];
+        wind = weather.hourly.windspeed_10m[currentHour];
+
         let weatherCode = weather.hourly.weathercode[currentHour];
-
-        switch (true) {
-            case (weatherCode === 0):
-                console.log('Clear sky')
-                break;
-
-            case (weatherCode >= 1 && weatherCode <= 3):
-                // 1, 2, 3	Mainly clear, partly cloudy, and overcast
-                console.log('Cloudy')
-                break;
-
-            case (weatherCode >= 45 && weatherCode <= 48):
-                console.log('Fog')
-                break;
-
-            case (weatherCode >= 51 && weatherCode <= 57):
-                console.log('Drizzle')
-                break;
-
-            case (weatherCode >= 61 && weatherCode <= 67):
-                console.log('Raining')
-                break;
-
-            case (weatherCode >= 80 && weatherCode <= 82):
-                console.log('Raining')
-                break;
-
-            case (weatherCode >= 71 && weatherCode <= 77):
-                console.log('Snowing')
-                break;
-
-            case (weatherCode >= 85 && weatherCode <= 86):
-                console.log('Snowing')
-                break;
-            
-            case (weatherCode >= 95 && weatherCode <= 99):
-                console.log('Thunderstorm')
-                break;
-
-            default:  
-                break;
-        }
+        Tweather =  Weathertoday(weatherCode);
     }
+
 
     return(
         <section className='cardSec'>
             <div className='allContent'>
                 <div className='card'>
-                    <div className='top'>
-                        <label className='labelSearchBar' htmlFor="searchBar">
-                            <input className='searchBar' id='searchBar' type="text" spellCheck="true" />
-                            <span className='searchIcon'><i className="fa-solid fa-magnifying-glass"></i></span>
-                        </label>
-                    </div>
                     
-                    <div className='middle'>
-                        <div className='temperature'>
-                            <span>{temperature}</span>
+                    <div className='top'>
+
+                        <form className='searchCity' onSubmit={handleSubmit}>
+                            <label className='labelSearchBar' htmlFor="searchBar">
+                                <input className='searchBar' value={city} onChange={handleChange} id='searchBar' type="text" autoComplete="off" placeholder='City' style={{ textTransform: "capitalize" }}/>
+                                <button type="submit" className='searchBtn'><i className="fa-solid fa-magnifying-glass"></i></button>
+                            </label>
+                        </form>
+
+                        <div className='middle'>
+                            <div className='weatherAnimation'>{Tweather}</div>
                         </div>
 
-                        <div className='weatherAnimation'>
-                            
-                            <div className='suny' style={{ display: clear }}>
-                                <div className="forecast">
-                                    <div className="forecast__sunny"></div>
-                                </div>
-                            </div>
+                        <div className='weatherInfo'>   
+                            <span className='cityContent'> <i className="fa-solid fa-location-dot"></i> {cityName} </span>
 
-                            <div className='cloudy' style={{ display: cloudy }}>
-                                <div className="forecast">
-                                    <div className="forecast__cloudy">
-                                        <div className="forecast__cloudy__sun"></div>
-                                        <div className="forecast__cloudy__cloud forecast__cloudy__cloud--small">
-                                        </div>
-                                        <div className="forecast__cloudy__cloud forecast__cloudy__cloud--normal">
-                                        </div>
-                                    </div>
+                            <div className='temperature-wind-drop'>
+                                <span className='temperature'> <i className="fa-solid fa-temperature-half"></i> {temperature}</span>
+                                <div className='wind-drop'>
+                                    <span className='wind'> <i className="fa-solid fa-wind"></i> {wind}</span>
+                                    <span className='droplet'> <i className="fa-solid fa-droplet"></i> {humidity}</span>
                                 </div>
-                            </div>
-
-                            <div className='rain' style={{ display: raining }}>
-                                <div className="forecast">
-                                    <div className="forecast__rainy">
-                                        <div className="forecast__rainy__rain forecast__rainy__rain--one">
-                                        </div>
-                                        <div className="forecast__rainy__rain forecast__rainy__rain--two">
-                                        </div>
-                                        <div className="forecast__rainy__rain forecast__rainy__rain--three">
-                                        </div>
-                                        <div className="forecast__rainy__rain forecast__rainy__rain--four">
-                                        </div>
-                                        <div className="forecast__rainy__rain forecast__rainy__rain--five">
-                                        </div>
-                                        <div className="forecast__rainy__rain forecast__rainy__rain--six">
-                                        </div>
-                                        <div className="forecast__rainy__rain forecast__rainy__rain--seven">
-                                        </div>
-                                        <div className="forecast__rainy__cloud forecast__rainy__cloud--grey"> 
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className='snow' style={{ display: snowing }}>
-                                <div className="forecast">
-                                    <div className="forecast__snow">
-                                        <div className="forecast__snow__snow forecast__snow__snow--first"><i className="fa-regular fa-snowflake"></i></div>
-                                        <div className="forecast__snow__snow forecast__snow__snow--second"><i className="fa-regular fa-snowflake"></i></div>
-                                        <div className="forecast__snow__snow forecast__snow__snow--third"><i className="fa-regular fa-snowflake"></i></div>
-                                        <div className="forecast__snow__snow forecast__snow__snow--fourth"><i className="fa-regular fa-snowflake"></i></div>
-                                        <div className="forecast__snow__snow forecast__snow__snow--five"><i className="fa-regular fa-snowflake"></i></div>
-                                        <div className="forecast__snow__snow forecast__snow__snow--six"><i className="fa-regular fa-snowflake"></i></div>
-                                        <div className="forecast__snow__snow forecast__snow__snow--seven"><i className="fa-regular fa-snowflake"></i></div>
-                                        <div className="forecast__snow__cloud forecast__snow__cloud--grey"></div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className='storm' style={{ display: 'block' }}>
-                                <div className="forecast">
-                                    <div className='bowie'></div>
-                                    <div className="forecast__cloudy">
-                                        <div className="forecast__cloudy__cloud forecast__cloudy__cloud--normal">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
+                            </div>   
                         </div>
-                    </div>
 
-                    <div className='bottom'>
-                        
+                        <div className='futureWeather'>
+                            {Fweather}
+                        </div>
+
                     </div>
                 </div>
             </div>
---
+++ src/components/Weathertoday.jsx
@@ -0,0 +1,184 @@
+export default function Weathertoday(weatherCode){
+    
+    if(weatherCode === undefined){
+        return;
+    }
+
+    switch (true) {
+        case (weatherCode === 0):
+            return(
+                <div className='clear'>
+                    <div className="forecast">
+                        <div className="forecast__sunny"></div>
+                    </div>
+                </div>
+            )
+        break;
+
+        case (weatherCode >= 1 && weatherCode <= 3):
+            return(
+                <div className='cloudy'>
+                    <div className="forecast">
+                        <div className="forecast__cloudy">
+                            <div className="forecast__cloudy__sun"></div>
+                            <div className="forecast__cloudy__cloud forecast__cloudy__cloud--small">
+                            </div>
+                            <div className="forecast__cloudy__cloud forecast__cloudy__cloud--normal">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        break;
+
+        case (weatherCode >= 45 && weatherCode <= 48):
+            return(
+                <div className='fog'>
+                    <div className="forecast">
+                        <div className="forecast__snow">
+                            <div className="forecast__fog__cloud"></div>
+                            <div className="inner"></div>
+                        </div>
+                    </div>
+                </div>
+            )
+        break;
+
+        case (weatherCode >= 51 && weatherCode <= 57):
+            return(
+                <div className='rain'>
+                    <div className="forecast">
+                        <div className="forecast__rainy">
+                            <div className="forecast__rainy__rain forecast__rainy__rain--one">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--two">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--three">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--four">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--five">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--six">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--seven">
+                            </div>
+                            <div className="forecast__rainy__cloud forecast__rainy__cloud--grey"> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        break;
+
+        case (weatherCode >= 61 && weatherCode <= 67):
+            return(
+                <div className='rain'>
+                    <div className="forecast">
+                        <div className="forecast__rainy">
+                            <div className="forecast__rainy__rain forecast__rainy__rain--one">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--two">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--three">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--four">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--five">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--six">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--seven">
+                            </div>
+                            <div className="forecast__rainy__cloud forecast__rainy__cloud--grey"> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        break;
+
+        case (weatherCode >= 80 && weatherCode <= 82):
+            return(
+                <div className='rain'>
+                    <div className="forecast">
+                        <div className="forecast__rainy">
+                            <div className="forecast__rainy__rain forecast__rainy__rain--one">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--two">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--three">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--four">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--five">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--six">
+                            </div>
+                            <div className="forecast__rainy__rain forecast__rainy__rain--seven">
+                            </div>
+                            <div className="forecast__rainy__cloud forecast__rainy__cloud--grey"> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        break;
+
+        case (weatherCode >= 71 && weatherCode <= 77):
+            return(
+                <div className='snow'>
+                    <div className="forecast">
+                        <div className="forecast__snow">
+                            <div className="forecast__snow__snow forecast__snow__snow--first"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--second"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--third"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--fourth"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--five"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--six"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--seven"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__cloud forecast__snow__cloud--grey"></div>
+                        </div>
+                    </div>
+                </div>
+            )
+        break;
+
+        case (weatherCode >= 85 && weatherCode <= 86):
+            return(
+                <div className='snow'>
+                    <div className="forecast">
+                        <div className="forecast__snow">
+                            <div className="forecast__snow__snow forecast__snow__snow--first"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--second"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--third"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--fourth"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--five"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--six"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__snow forecast__snow__snow--seven"><i className="fa-regular fa-snowflake"></i></div>
+                            <div className="forecast__snow__cloud forecast__snow__cloud--grey"></div>
+                        </div>
+                    </div>
+                </div>
+            )
+        break;
+        
+        case (weatherCode >= 95 && weatherCode <= 99):
+            return(
+                <div className='storm'>
+                    <div className="forecast">
+                        <div className='bowie'></div>
+                        <div className="forecast__cloudy">
+                            <div className="forecast__cloudy__cloud forecast__cloudy__cloud--normal">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        break;
+
+        default:  
+            break;
+    }
+
+}
